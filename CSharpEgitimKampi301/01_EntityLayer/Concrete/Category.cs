891a10fb5c386ece1614b960924b25ace683014f
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-11-16 19:00:49 +0300

    lesson 12 completed

--- CSharpEgitimKampi301/01_EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301/01_EntityLayer/Concrete/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace _01_EntityLayer.Concrete
+﻿using System.Collections.Generic;
+
+namespace _01_EntityLayer.Concrete
 {
 	public class Category
 	{
 		public int CategoryId { get; set; } // CodeFirst yaklaşımında primary key olarak alınması için entityNameId şeklinde olmalıdır.
 		public string CategoryName { get; set; }
 		public bool CategoryStatus { get; set; }
+		public List<Product> Products { get; set; }
 	}
 }
