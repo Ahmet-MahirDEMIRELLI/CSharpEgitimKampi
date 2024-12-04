8abc42049581517de851cb835b3ee9053e59d4b2
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-12-04 21:41:10 +0300

    Lesson 19 completed

---
+++ CSharpEgitimKampi301/01_BusinessLayer/Abstarct/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using _01_EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_BusinessLayer.Abstarct
+{
+	public interface IOrderService : IGenericService<Order>
+	{
+	}
+}
