c716c16f4da36aad74d5877e2746b25376e38d62
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-12-25 20:35:06 +0300

    Lesson 24 completed

---
+++ CSharpEgitimKampi601/Services/CustomerOperations.cs
@@ -0,0 +1,28 @@
+﻿using CSharpEgitimKampi601.Entities;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi601.Services
+{
+	public class CustomerOperations
+	{
+		public void AddCustomer(Customer customer)
+		{
+			var connection = new MongoDbConnection();
+			var customerCollection = connection.GetCustomersCollection();
+			var document = new BsonDocument
+			{
+				{"Name", customer.Name},
+				{"Surname", customer.Surname},
+				{"City", customer.City},
+				{"Balance", customer.Balance},
+				{"OrderCount", customer.OrderCount}
+			};
+			customerCollection.InsertOne(document);
+		}
+	}
+}
